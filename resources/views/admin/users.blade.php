30df4f6f61fdaa7f43d726b9121e5772c8ec176a
Mark John <markjohndatz@gmail.com>
2023-06-28 11:11:25 +0800

    API-user-update

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -2,52 +2,43 @@
 
 @section('content')
 
-<div class="container">
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Image</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // Assuming you have already set up your database connection in Laravel
+<div class="container py-5">
+    <div class="row">
+        <?php
+        // Assuming you have already set up your database connection in Laravel
 
-            $users = DB::table('users')->select('id', 'name', 'email', 'role_as')->get();
+        $users = DB::table('users')->select('id', 'name', 'email', 'role_as')->get();
+
+        foreach ($users as $user) {
+            echo '<div class="col-md-3 mb-3">';
+            echo '<div class="card shadow-small">';
+            echo '<img src="' . fetchImage($user->id) . '" class="card-img-top" alt="User Image">';
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title">' . $user->name . '</h5>';
+            echo '<p class="card-text">Email: ' . $user->email . '</p>';
+            echo '<p class="card-text">Role: ' . ($user->role_as == 1 ? 'Admin' : 'User') . '</p>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+
+        function fetchImage($userId)
+        {
+            $url = 'https://reqres.in/api/users?fbclid=IwAR12Wd3sMaCXoCDUuugkkFFC04qVHXIHXk13uunj07ZffYqpqwNsrRLweXU';
+            $data = file_get_contents($url);
+            $jsonData = json_decode($data, true);
+            $users = $jsonData['data'];
 
             foreach ($users as $user) {
-                echo '<tr>';
-                echo '<td>' . $user->id . '</td>';
-                echo '<td>' . $user->name . '</td>';
-                echo '<td>' . $user->email . '</td>';
-                echo '<td>' . ($user->role_as == 1 ? 'Admin' : 'User') . '</td>';
-                echo '<td><img src="' . fetchImage($user->id) . '" alt="User Image" width="100"></td>';
-                echo '</tr>';
-            }
-
-            function fetchImage($userId)
-            {
-                $url = 'https://reqres.in/api/users?fbclid=IwAR12Wd3sMaCXoCDUuugkkFFC04qVHXIHXk13uunj07ZffYqpqwNsrRLweXU';
-                $data = file_get_contents($url);
-                $jsonData = json_decode($data, true);
-                $users = $jsonData['data'];
-
-                foreach ($users as $user) {
-                    if ($user['id'] == $userId) {
-                        return $user['avatar'];
-                    }
+                if ($user['id'] == $userId) {
+                    return $user['avatar'];
                 }
-
-                return '';
             }
-            ?>
-        </tbody>
-    </table>
+
+            return '';
+        }
+        ?>
+    </div>
 </div>
 
 @endsection
-
